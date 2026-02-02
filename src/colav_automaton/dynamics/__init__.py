f655b4564465cd88b9b36fb57509fbb49980383d
michael <mstolbergrer01@qub.ac.uk>
2026-02-02 17:30:33 +0000

    Additional Updates

--- src/colav_automaton/dynamics/__init__.py
+++ src/colav_automaton/dynamics/__init__.py
@@ -1,8 +1,3 @@
-__author__ = "Ryan McKee <r.mckee@liverpool.ac.uk>"
-__version__ = "0.0.1"
-__description__ = "dynamics functions for the states of hybrid-automaton colav-automaton" \
-                  ""
-
 from .dynamics import (
     S1_waypoint_reaching_dynamics,
     S2_collision_avoidance_dynamics,
